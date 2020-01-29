c5d9de1f68acc3a103e7d2e7346ce37147f8172e
Gregor Kappler <gregor.kappler@bictxt.de>
2020-01-29 11:03:55 +0100

    Files generated by PkgTemplates

---
+++ src/LabeledArrays.jl
@@ -0,0 +1,5 @@
+module LabeledArrays
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LabeledArrays
+using Test
+
+@testset "LabeledArrays.jl" begin
+    # Write your own tests here.
+end
