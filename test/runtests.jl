77d1cccd36865f8c5c212e76bb1af8344af06f89
Gregor Kappler <gregor.kappler@bictxt.de>
2020-07-09 14:43:54 +0200

    some tests for NBijection

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,17 @@ using Test
 
 @testset "LabeledArrays.jl" begin
     # Write your own tests here.
+    @testset "NBijection" begin
+        x = NBijection('a':'z')
+        @test x[13] == 'm'
+        @test eltype(x) == Char
+        @test collect(x) == vcat('a':'z',['ö'])
+        @test get!(x,'m') == 13
+        @test get!(x,'ö') == 27
+        @test keytype(x) == Char
+        @test keys(x) == vcat('a':'z',['ö'])
+        @test valtype(x) == Int
+        @test values(x) == 1:27
+        @test indexin('m', x) == indexin('m', collect(x))
+    end
 end
